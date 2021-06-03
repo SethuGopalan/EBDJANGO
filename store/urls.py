46436ba3ea492e16382d82d8f6d60bfa1aed1afa
Sethu Gopalan <Sethu.Gopalan@cytel.com>
2021-06-03 19:59:34 -0400

    add home page

---
+++ store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+
+from . import views
+
+app_name = 'store'
+
+urlpatterns = [
+    path('', views.all_products, name ='all_products')
+    
+]
